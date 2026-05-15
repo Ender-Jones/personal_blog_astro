f8df0be1c2bf319d84db0f7abe8e7e8b22a9c9a3
Ender-Jones <111357076+Ender-Jones@users.noreply.github.com>
2026-05-15 20:19:56 +0900

    chore: trim homepage support UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import yaml from '@rollup/plugin-yaml';
 export default defineConfig({
   site: 'https://enderjones.com',
   output: 'static',
+  devToolbar: {
+    enabled: false,
+  },
   integrations: [mdx()],
   vite: {
     plugins: [yaml()],
